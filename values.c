6c1f9d43a97be0922a47c10ef30faf727c6a5e4c
VaishnaviVanka <vaishuvanka6900@gmail.com>
2024-10-30 15:11:31 +0530

    Create values.c

---
+++ values.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the n value:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+    printf("positive");
+    }
+    else if(n==0)
+    {
+    printf("zero");
+    }
+    else 
+    {
+     printf("negative");
+    }
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
